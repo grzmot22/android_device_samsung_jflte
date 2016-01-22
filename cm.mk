a75ff8ee467d3458997b1c25de36881ee1936f8d
grzmot22 <grzmot22@gmail.com>
2016-01-22 16:03:02 +0100

    Remove old V4A libs
    Change-Id: I197b75d6a698bb1f05e2eb73227f9755f67c031e

--- cm.mk
+++ cm.mk
@@ -9,7 +9,4 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common JDC stuff.
 $(call inherit-product, vendor/jdc/config/common.mk)
 
-#CM (V4A) Audio Mod		
-CM_AUDIO_MOD := viper
-
 PRODUCT_NAME := cm_jflte
